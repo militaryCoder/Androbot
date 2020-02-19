38cf4f9d8bdaff43b18c2b9621cb8af0f8738308
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2020-02-19 17:03:08 +0300

    Added Cmake build system

---
+++ CMake/AddLibrealsense.cmake
@@ -0,0 +1,17 @@
+include(ExternalProject)
+
+if(NOT EXISTS $ENV{LIBREALSENSE2_SOURCE_DIR})
+    set($ENV{LIBREALSENSE2_SOURCE_DIR}
+        ${CMAKE_CURRENT_SOURCE_DIR}/3rd-party/librealsense2)
+
+    ExternalProject_Add(librealsense2
+        PREFIX librealsense2
+        GIT_REPOSITORY https://github.com/IntelRealSense/librealsense.git
+        SOURCE_DIR $ENV{LIBREALSENSE2_SOURCE_DIR}
+        BINARY_DIR $ENV{LIBREALSENSE2_SOURCE_DIR}/bin
+    )
+endif()
+
+# if(WIN32)
+#     set(LIBREALSENSE2_LIBRARIES ${librealsense2_BINARY_DIR}/)
+# endif()
