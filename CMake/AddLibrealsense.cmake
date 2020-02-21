8877c420b3e3bf6ea859e8cc7304df0d69f20c73
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2020-02-21 21:09:44 +0300

    Modified librealsense build module

--- CMake/AddLibrealsense.cmake
+++ CMake/AddLibrealsense.cmake
@@ -1,14 +1,23 @@
 include(ExternalProject)
 
+set($ENV{LIBREALSENSE2_SOURCE_DIR} ${CMAKE_SOURCE_DIR}/3rd-party/librealsense2)
+set(LIBREALSENSE_BUILD_COMMAND "mkdir "${CMAKE_BINARY_DIR}"/librealsense; cd librealsense; cmake "$ENV{LIBREALSENSE2_SOURCE_DIR}; "cmake --build .")
+
 if(NOT EXISTS $ENV{LIBREALSENSE2_SOURCE_DIR})
-    set($ENV{LIBREALSENSE2_SOURCE_DIR}
-        ${CMAKE_CURRENT_SOURCE_DIR}/3rd-party/librealsense2)
+    if(NOT EXISTS ${CMAKE_BINARY_DIR}/librealsense)
+        set(LIBREALSENSE_SHOULD_BUILD TRUE)
 
     ExternalProject_Add(librealsense2
         PREFIX librealsense2
         GIT_REPOSITORY https://github.com/IntelRealSense/librealsense.git
         SOURCE_DIR $ENV{LIBREALSENSE2_SOURCE_DIR}
         BINARY_DIR ${CMAKE_BINARY_DIR}/librealsense
+
+        UPDATE_COMMAND ${GIT_EXECUTABLE} pull origin master
+
+        BUILD_COMMAND $<${LIBREALSENSE_SHOULD_BUILD}:${LIBREALSENSE_BUILD_COMMAND}>
+        BUILD_ALWAYS OFF
+        BUILD_IN_SOURCE OFF
     )
 endif()
 
